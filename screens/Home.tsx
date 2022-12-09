0610d961c3fbbec58ccdb1c0e2b3ef7fc9596c92
softone-as <shofwanas12@gmail.com>
2022-12-09 11:10:35 +0700

    Slicing card components

--- screens/Home.tsx
+++ screens/Home.tsx
@@ -1,7 +1,12 @@
+import { StatusBar } from 'expo-status-bar';
 import { FunctionComponent } from 'react';
 import styled from 'styled-components/native';
+import CardSection from '../components/Cards/CardSection';
 import { colors } from '../components/colors';
 import { Container } from '../components/shared';
+
+import logo1 from '../assets/cards/mc.png';
+import logo2 from '../assets/cards/visa_white.png';
 
 const HomeContainer = styled(Container)`
     background-color: ${colors.graylight};
@@ -10,7 +15,35 @@ const HomeContainer = styled(Container)`
 `;
 
 const Home: FunctionComponent = () => {
-    return <HomeContainer></HomeContainer>;
+    const cardData = [
+        {
+            id: 1,
+            accountNo: '38674140534',
+            balance: 575000.12,
+            alias: 'Work Debit',
+            logo: logo1,
+        },
+        {
+            id: 2,
+            accountNo: '3867465286',
+            balance: 120000.23,
+            alias: 'Personal Prepaid',
+            logo: logo2,
+        },
+        {
+            id: 3,
+            accountNo: '3866243597',
+            balance: 57000.12,
+            alias: 'School Prepaid',
+            logo: logo1,
+        },
+    ];
+    return (
+        <HomeContainer>
+            <StatusBar style='dark' />
+            <CardSection data={cardData} />
+        </HomeContainer>
+    );
 };
 
 export default Home;
